ddab834957ed5e033d8e81f9da253004f572a10a
Jassar Al_Akhars <902703@mof.gov.sa>
2020-02-03 16:15:14 +0300

    EmployeeEdit View

---
+++ BethanysPieShopHR.Server/Pages/EmployeeEditBase.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShopHR.Server.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHR.Server.Pages
+{
+    public class EmployeeEditBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeDataService _employeeDataService { get; set; }
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await _employeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+    }
+}

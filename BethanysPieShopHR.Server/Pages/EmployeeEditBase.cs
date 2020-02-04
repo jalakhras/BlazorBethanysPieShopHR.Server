61d4d8a1f3e4851786694b86820d99b803864dea
Jassar Al_Akhars <902703@mof.gov.sa>
2020-02-04 15:33:30 +0300

    Adding the Add Employee Form

--- BethanysPieShopHR.Server/Pages/EmployeeEditBase.cs
+++ BethanysPieShopHR.Server/Pages/EmployeeEditBase.cs
@@ -1,6 +1,8 @@
 ﻿using BethanysPieShopHR.Server.Services;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BethanysPieShopHR.Server.Pages
@@ -8,15 +10,27 @@ namespace BethanysPieShopHR.Server.Pages
     public class EmployeeEditBase : ComponentBase
     {
         [Inject]
-        public IEmployeeDataService _employeeDataService { get; set; }
+        public IEmployeeDataService _employeeDataService { get; set; }  
+        [Inject]
+        public ICountryDataService _countryDataService { get; set; }
+        [Inject]
+        public IJobCategoryDataService _jobCategoryDataService { get; set; }
         [Parameter]
         public string EmployeeId { get; set; }
 
         public Employee Employee { get; set; } = new Employee();
+        public List<Country> Countries { get; set; } = new List<Country>();
+        public List<JobCategory> JobCategory { get; set; } = new List<JobCategory>();
 
+        public string CountryId = string.Empty;
+        public string JobCategoryId = string.Empty;
         protected async override Task OnInitializedAsync()
         {
             Employee = await _employeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            Countries = (await _countryDataService.GetAllCountries()).ToList();
+            CountryId = Employee.CountryId.ToString();
+            JobCategory = (await _jobCategoryDataService.GetAllJobCategories()).ToList();
+            JobCategoryId = Employee.JobCategoryId.ToString();
         }
     }
 }
--- BethanysPieShopHR.Server/Startup.cs
+++ BethanysPieShopHR.Server/Startup.cs
@@ -23,10 +23,11 @@ namespace BethanysPieShopHR.Server
         {
             services.AddRazorPages();
             services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(
-                client=> { client.BaseAddress = new Uri("https://localhost:44340/"); }
-               
-
-                ); 
+                client=> { client.BaseAddress = new Uri("https://localhost:44340/"); }); 
+            services.AddHttpClient<ICountryDataService, CountryDataService>(
+                client=> { client.BaseAddress = new Uri("https://localhost:44340/"); });
+            services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(
+                client=> { client.BaseAddress = new Uri("https://localhost:44340/"); }); 
             services.AddServerSideBlazor().AddCircuitOptions(options=> { options.DetailedErrors = true; });
         }
 
